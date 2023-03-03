f27c64bba4a02e42c8a1765de180428bd9f4b5ab
Demid <90619714+HEu3BECTHbIU@users.noreply.github.com>
2023-03-04 00:37:45 +0500

    Task_1

---
+++ Task_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <gl/glut.h>
+#include <iostream>
+
+void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Task 1");
+
+    glutDisplayFunc(RenderSceneCB);
+    glClearColor(0.0f, 1.0f, 0.0f, 0.5f);
+    glutMainLoop();
+
+}
