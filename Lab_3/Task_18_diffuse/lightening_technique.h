d4adff71fb1185dee8e78cede1f806913b923e2b
Demid <demidberdin1234@gmail.com>
2023-04-20 09:21:47 +0500

    Task 18 added

---
+++ Lab_3/Task_18_diffuse/lightening_technique.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "technique.h"
+#include "math_3d.h"
+
+struct DirectionLight // направленный свет
+{
+    Vector3f Color; // цвет
+    float AmbientIntensity; // интенсивность фонового света
+    Vector3f Direction; // направление луча
+    float DiffuseIntensity; // интенсивность диффузного света
+};
+
+class LightingTechnique : public Technique // класс техники света
+{
+public:
+    LightingTechnique();
+    virtual bool Init();
+
+    void SetWVP(const Matrix4f* WVP);
+    void SetTextureUnit(unsigned int TextureUnit);
+    void SetDirectionalLight(const DirectionLight& Light);
+    void SetWorldMatrix(const Matrix4f& WVP);
+
+private:
+    GLuint m_WVPLocation;
+    GLuint m_WorldMatrixLocation;
+    GLuint m_samplerLocation;
+
+    struct {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint Direction;
+        GLuint DiffuseIntensity;
+    } m_dirLightLocation;
+};
