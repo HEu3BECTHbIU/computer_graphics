9cc6d76104302dd83852e88ba5765b5672f85243
Demid <demidberdin1234@gmail.com>
2023-04-20 09:25:56 +0500

    Task 20 added

---
+++ Lab_3/Task_20_light_source/lightening_technique.h
@@ -0,0 +1,98 @@
+﻿#pragma once
+
+#include "technique.h"
+#include "math_3d.h"
+
+#define MAX_POINT_LIGHTS 3
+#define INVALID_UNIFORM_LOCATION 0xFFFFFFFF
+struct BaseLight
+{
+    Vector3f Color; // цвет света
+    float AmbientIntensity; // интенсивность фонового
+    float DiffuseIntensity; // инетнсивность рассеянного
+
+    BaseLight()
+    {
+        Color = Vector3f(0.0f, 0.0f, 0.0f);
+        AmbientIntensity = 0.0f;
+        DiffuseIntensity = 0.0f;
+    }
+};
+
+struct PointLight : public BaseLight // точечный свет
+{
+    Vector3f Position;
+
+    struct
+    {
+        float Constant; // коэффициенты затухания
+        float Linear;
+        float Exp;
+    } Attenuation;
+
+    PointLight()
+    {
+        Position = Vector3f(0.0f, 0.0f, 0.0f);
+        Attenuation.Constant = 1.0f;
+        Attenuation.Linear = 0.0f;
+        Attenuation.Exp = 0.0f;
+    }
+};
+struct DirectionalLight : public BaseLight // направленный свет
+{
+    Vector3f Direction; // направление
+
+    DirectionalLight()
+    {
+        Direction = Vector3f(0.0f, 0.0f, 0.0f);
+    }
+};
+
+class LightingTechnique : public Technique // класс техники света
+{
+public:
+    LightingTechnique();
+    virtual bool Init();
+
+    void SetWVP(const Matrix4f* WVP);
+    void SetTextureUnit(unsigned int TextureUnit);
+    void SetDirectionalLight(const DirectionalLight& Light);
+    void SetWorldMatrix(const Matrix4f& WVP);
+
+    void SetEyeWorldPos(const Vector3f& EyeWorldPos);
+    void SetMatSpecularIntensity(float Intensity);
+    void SetMatSpecularPower(float Power);
+    void SetPointLights(unsigned int NumLights, const PointLight* pLights);
+
+private:
+    GLuint m_WVPLocation;
+    GLuint m_WorldMatrixLocation;
+    GLuint m_samplerLocation;
+
+    GLuint m_eyeWorldPosition;
+    GLuint m_matSpecularIntensityLocation;
+    GLuint m_matSpecularPowerLocation;
+
+    GLuint m_numPointLightsLocation;
+    struct 
+    {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint Direction;
+        GLuint DiffuseIntensity;
+    } m_dirLightLocation;
+
+    struct
+    {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint DiffuseIntensity;
+        GLuint Position;
+        struct
+        {
+            GLuint Constant;
+            GLuint Linear;
+            GLuint Exp;
+        } Atten;
+    } m_pointLightsLocation[MAX_POINT_LIGHTS];
+};
---
+++ Lab_3/Task_20_light_source/technique.cpp
@@ -0,0 +1,142 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+#include "technique.h"
+
+Technique::Technique() 
+{
+    m_shaderProg = 0;
+}
+
+Technique::~Technique() // десткуктор
+{
+    // удаляем шейдеры для оптимизации
+    for (ShaderObjList::iterator it = m_shaderObjList.begin(); it != m_shaderObjList.end(); it++)
+    {
+        glDeleteShader(*it);
+    }
+    // удаляем шейдерную программу
+    if (m_shaderProg != 0)
+    {
+        glDeleteProgram(m_shaderProg);
+        m_shaderProg = 0;
+    }
+}
+
+bool Technique::Init() // функция создания шейдерной программы
+{
+    m_shaderProg = glCreateProgram();
+
+    if (m_shaderProg == 0) 
+    {
+        fprintf(stderr, "Error creating shader program\n");
+        return false;
+    }
+
+    return true;
+}
+
+//Используем этот метод для добавления шейдеров в программу. Когда заканчиваем - вызываем finalize()
+bool Technique::AddShader(GLenum ShaderType, const char* pShaderText)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType); // создание шейдерного объекта
+
+    if (ShaderObj == 0) 
+    {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        return false;
+    }
+    // Сохраним объект шейдера - он будет удален в декструкторе
+    m_shaderObjList.push_back(ShaderObj);
+
+    const GLchar* p[1];
+    p[0] = pShaderText; // код шейдера (программа)
+    GLint Lengths[1];
+    Lengths[0] = strlen(pShaderText); // длина программы шейдера
+    glShaderSource(ShaderObj, 1, p, Lengths); // привязка данных шейдера к шейдерному объекту
+
+    glCompileShader(ShaderObj); // компиляция шейдера
+
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);// проверка компиляции
+    if (!success)  // в случае неудачной компиляции (ошибки в коде шейдера)
+    {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        return false;
+    }
+    glAttachShader(m_shaderProg, ShaderObj); //привязка шейдерного объекта к программному
+
+    return true;
+}
+// После добавления всех шейдеров в программу вызываем эту функцию
+// для линковки и проверки программу на ошибки
+bool Technique::Finalize() 
+{
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(m_shaderProg); // линковка программы (создание исполняемого файла) 
+
+    glGetProgramiv(m_shaderProg, GL_LINK_STATUS, &Success); // проверка успешности линковки
+    if (Success == 0) 
+    {
+        glGetProgramInfoLog(m_shaderProg, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        return false;
+    }
+
+    glValidateProgram(m_shaderProg); // проверка возможности запуска программы
+    glGetProgramiv(m_shaderProg, GL_VALIDATE_STATUS, &Success);
+    if (Success == 0) 
+    {
+        glGetProgramInfoLog(m_shaderProg, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        return false;
+    }
+    // Удаляем промежуточные объекты шейдеров, которые были добавлены в программу
+    for (ShaderObjList::iterator it = m_shaderObjList.begin(); it != m_shaderObjList.end(); it++)
+    {
+        glDeleteShader(*it);
+    }
+    m_shaderObjList.clear();
+
+    return true;
+}
+
+void Technique::Enable() 
+{
+    glUseProgram(m_shaderProg); // назначение шейдерной программы для использования в конвейере
+}
+
+GLint Technique::GetUniformLocation(const char* pUniformName)
+{
+    GLint Location = glGetUniformLocation(m_shaderProg, pUniformName);
+
+    if (Location == 0xFFFFFFFF) 
+    {
+        fprintf(stderr, "Warning! Unable to get the location of uniform '%s'\n", pUniformName);
+    }
+
+    return Location;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
