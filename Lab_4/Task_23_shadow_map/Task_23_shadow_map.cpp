81302850707e2b9d66386b16019a650cd85d8956
Demid <demidberdin1234@gmail.com>
2023-05-01 18:37:10 +0500

    Task 23 added

---
+++ Lab_4/Task_23_shadow_map/Task_23_shadow_map.cpp
@@ -0,0 +1,218 @@
+﻿#include <stdio.h>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <math.h>
+#include "pipeline.h"
+#include "camera.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "texture.h"
+#include "lightening_technique.h"
+#include "glut_backend.h"
+#include "util.h"
+#include "mesh.h"
+#include "shadow_map_fbo.h"
+#include "shadow_map_technique.h"
+
+#define WINDOW_WIDTH  1920
+#define WINDOW_HEIGHT 1080
+
+
+class Main : public ICallbacks
+{
+public:
+
+    Main()
+    {
+        m_pGameCamera = NULL; 
+        m_pShadowMapTech = NULL; 
+        m_pMesh = NULL; 
+        m_pQuad = NULL; 
+        m_pEffect = NULL; 
+        m_scale = 0.0f;
+        // свет типа прожектор
+        m_spotLight.AmbientIntensity = 0.0f;
+        m_spotLight.DiffuseIntensity = 0.9f;
+        m_spotLight.Color = Vector3f(1.0f, 1.0f, 1.0f);
+        m_spotLight.Attenuation.Linear = 0.01f;
+        m_spotLight.Position = Vector3f(-20.0, 20.0, 5.0f);
+        m_spotLight.Direction = Vector3f(1.0f, -1.0f, 0.0f);
+        m_spotLight.Cutoff = 20.0f;
+    }
+
+    ~Main()
+    {
+        SAFE_DELETE(m_pEffect);
+        SAFE_DELETE(m_pShadowMapTech);
+        SAFE_DELETE(m_pGameCamera);
+        SAFE_DELETE(m_pMesh);
+        SAFE_DELETE(m_pQuad);
+    }
+
+    bool Init()
+    {
+        if (!m_shadowMapFBO.Init(WINDOW_WIDTH, WINDOW_HEIGHT)) 
+        {
+            return false;
+        }
+        // инициализация камеры 
+        m_pGameCamera = new Camera(WINDOW_WIDTH / 1.25, WINDOW_HEIGHT / 1.25);
+
+        m_pEffect = new LightingTechnique();// инициализация света
+
+        if (!m_pEffect->Init())
+        {
+            printf("Error initializing the lighting technique\n");
+            return false;
+        }
+        m_pShadowMapTech = new ShadowMapTechnique(); // инициализация теней
+
+        if (!m_pShadowMapTech->Init()) 
+        {
+            printf("Error initializing the shadow map technique\n");
+            return false;
+        }
+        m_pShadowMapTech->Enable(); // назначение шейдерной программы для использования в конвейере
+        // m_pEffect->Enable();  // назначение шейдерной программы для использования в конвейере
+
+        //m_pEffect->SetTextureUnit(0); // передача в шейдер модуля текстуры (0)
+        m_pQuad = new Mesh(); // инициализация mesh модели куба
+        if (!m_pQuad->LoadMesh("quad.txt")) 
+        {
+            return false;
+        }
+        m_pMesh = new Mesh();
+        return m_pMesh->LoadMesh("phoenix.md2"); // загрузка меш модели
+    }
+
+    void Run() // запуск инициализации
+    {
+        GLUTBackendRun(this);
+    }
+
+    virtual void RenderSceneCB() // функция отрисовки
+    {
+        m_pGameCamera->OnRender();
+        m_scale += 0.05f;
+
+        ShadowMapPass(); // вызываем функцию для рендера в карту теней
+        RenderPass(); // отображение результата
+
+        glutSwapBuffers();
+    }
+
+    virtual void ShadowMapPass()
+    {
+        m_shadowMapFBO.BindForWriting(); // привязка FBO
+        //начиная с этого момента все значения высот будут переходить в нашу текстуру карты теней,
+        //  а цвет будет выброшен
+
+        glClear(GL_DEPTH_BUFFER_BIT); // очистка буфера глубины
+
+        Pipeline p;
+        p.Scale(0.2f, 0.2f, 0.2f);
+        p.Rotate(0.0f, m_scale, 0.0f); // поворот объекта вокруг оси
+        p.WorldPos(0.0f, 0.0f, 5.0f); // положение объекта
+        // установка векторов камеры
+        p.SetCamera(m_spotLight.Position, m_spotLight.Direction, Vector3f(0.0f, 1.0f, 0.0f));
+        // проекция перспективы
+        p.SetPerspectiveProj(60.0f, WINDOW_WIDTH, WINDOW_HEIGHT, 1.0f, 50.0f);
+        // передача матрицы трасформации в шейдер
+        m_pShadowMapTech->SetWVP(p.GetWVPTrans());
+        // рендер меша (модель из игры)
+        m_pMesh->Render();
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0); // переключаемся обратно в стандартный буфер кадра
+    }
+
+    virtual void RenderPass()
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // очистка буферов и цвета и глубины
+
+        m_pShadowMapTech->SetTextureUnit(0); // привязка модуля текстуры 0
+        m_shadowMapFBO.BindForReading(GL_TEXTURE0); // привязываем карту теней для чтения в модуле 0
+        // масштабирование квадрат, помещение перед камерой и рендер
+        Pipeline p;
+        p.Scale(5.0f, 5.0f, 5.0f);
+        p.WorldPos(0.0f, 0.0f, 10.0f);
+        p.SetCamera(m_pGameCamera->GetPos(), m_pGameCamera->GetTarget(), m_pGameCamera->GetUp());
+        p.SetPerspectiveProj(60.0f, WINDOW_WIDTH, WINDOW_HEIGHT, 1.0f, 50.0f);
+        m_pShadowMapTech->SetWVP(p.GetWVPTrans());
+        // отрисовка меша (куб)
+        m_pQuad->Render();
+    }
+
+    virtual void IdleCB()
+    {
+        RenderSceneCB();
+    }
+
+    virtual void SpecialKeyboardCB(int Key, int x, int y)
+    {
+        m_pGameCamera->OnKeyboard(Key);
+    }
+
+    virtual void KeyboardCB(unsigned char Key, int x, int y)
+    {
+        switch (Key)
+        {
+        case 'q': // закрытие программы
+            glutLeaveMainLoop();
+            break;
+
+        case 'a': // увеличение интенсивности фонового света
+            m_directionalLight.AmbientIntensity += 0.05f;
+            break;
+
+        case 's':// уменьшение интенсивности фонового света
+            m_directionalLight.AmbientIntensity -= 0.05f;
+            break;
+
+        case 'z': // увеличение интенсивности рассеяного света
+            m_directionalLight.DiffuseIntensity += 0.05f;
+            break;
+
+        case 'x': // уменьшение интенсивности рассеяного света
+            m_directionalLight.DiffuseIntensity -= 0.05f;
+            break;
+        }
+    }
+
+    virtual void PassiveMouseCB(int x, int y)
+    {
+        m_pGameCamera->OnMouse(x, y);
+    }
+
+private:
+    LightingTechnique* m_pEffect; // свет
+    ShadowMapTechnique* m_pShadowMapTech; // тени
+    Camera* m_pGameCamera; // камера
+    SpotLight m_spotLight; // прожектор
+    Mesh* m_pMesh; // меш модели
+    Mesh* m_pQuad; // меш квадрата
+    ShadowMapFBO m_shadowMapFBO; // карта теней
+    DirectionalLight m_directionalLight;
+    float m_scale;
+};
+
+int main(int argc, char** argv)
+{
+    GLUTBackendInit(argc, argv);
+
+    if (!GLUTBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, 60, true, "Task 23"))
+    {
+        return 1;
+    }
+
+    Main* pApp = new Main();
+
+    if (!pApp->Init()) 
+    {
+        return 1;
+    }
+
+    pApp->Run();
+
+    delete pApp;
+
+    return 0;
+}
---
+++ Lab_4/Task_23_shadow_map/mesh.cpp
@@ -0,0 +1,213 @@
+﻿#include <assert.h>
+#include <iostream>
+#include "mesh.h"
+
+Mesh::MeshEntry::MeshEntry() // обнуление переменных при инициализации
+{
+    VB = INVALID_OGL_VALUE;
+    IB = INVALID_OGL_VALUE;
+    NumIndices = 0;
+    MaterialIndex = INVALID_MATERIAL;
+}
+
+Mesh::MeshEntry::~MeshEntry() // деструктор (удаляет массивы)
+{
+    if (VB != INVALID_OGL_VALUE) 
+    {
+        glDeleteBuffers(1, &VB);
+    }
+
+    if (IB != INVALID_OGL_VALUE) 
+    {
+        glDeleteBuffers(1, &IB);
+    }
+}
+
+bool Mesh::MeshEntry::Init(const std::vector<Vertex>& Vertices,
+    const std::vector<unsigned int>& Indices)
+{
+    NumIndices = Indices.size();
+    // создание вершинного массива OpenGL на основе модели
+    glGenBuffers(1, &VB);
+    glBindBuffer(GL_ARRAY_BUFFER, VB);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(),
+        &Vertices[0], GL_STATIC_DRAW);
+    // создание индексного массива OpenGL на основе модели
+    glGenBuffers(1, &IB);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * NumIndices,
+        &Indices[0], GL_STATIC_DRAW);
+
+    return true;
+}
+
+void Mesh::Clear() // функция удаления указателей на объекты текстуры
+{
+    for (unsigned int i = 0; i < m_Textures.size(); i++)
+    {
+        SAFE_DELETE(m_Textures[i]);
+    }
+}
+// функция загрузки меша
+bool Mesh::LoadMesh(const std::string& Filename) 
+{
+    // Удаляем данные предыдущей модели (если она была загружена)
+    Clear();
+
+    bool Ret = false;
+    Assimp::Importer Importer;
+    // загрузка модели из файла (флаги - деление полигонов на треугольники, подсчет нормалей (если еще нет), поворот вокруг оси y)
+    const aiScene* pScene = Importer.ReadFile(Filename.c_str(),
+        aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs);
+    // если удачно, идем дальше
+    if (pScene) 
+    {
+        Ret = InitFromScene(pScene, Filename);
+    }
+    else {
+        printf("Error parsing '%s': '%s'\n", Filename.c_str(), Importer.GetErrorString());
+    }
+
+    return Ret;
+}
+// функция инициализации объекта меша
+bool Mesh::InitFromScene(const aiScene* pScene, const std::string& Filename)
+{
+    // изменям размер массивов в зависимости от количества мешей и текстур
+    m_Entries.resize(pScene->mNumMeshes);
+    m_Textures.resize(pScene->mNumMaterials);
+
+    // Инициализируем меши один за другим
+    for (unsigned int i = 0; i < m_Entries.size(); i++)
+    {
+        const aiMesh* paiMesh = pScene->mMeshes[i];
+        InitMesh(i, paiMesh);
+    }
+    return InitMaterials(pScene, Filename);
+}
+// иницилазиация одного меша
+void Mesh::InitMesh(unsigned int Index, const aiMesh* paiMesh)
+{
+    m_Entries[Index].MaterialIndex = paiMesh->mMaterialIndex;
+
+    std::vector<Vertex> Vertices; //вектор вершин
+    std::vector<unsigned int> Indices; // вектор индексов
+    std::cout << paiMesh->mNumVertices << "\n";
+    const aiVector3D Zero3D(0.0f, 0.0f, 0.0f);
+    //заполняем массивы используя соответствующие поля объекта aiMesh
+    for (unsigned int i = 0; i < paiMesh->mNumVertices; i++)
+    {
+        const aiVector3D* pPos = &(paiMesh->mVertices[i]); // позиция
+        const aiVector3D* pNormal = &(paiMesh->mNormals[i]); // нормаль
+        // кордината текстуры
+        const aiVector3D* pTexCoord = paiMesh->HasTextureCoords(0) ?
+            &(paiMesh->mTextureCoords[0][i]) : &Zero3D;
+        // собираем все вместе в объект структуры
+        Vertex v(Vector3f(pPos->x, pPos->y, pPos->z),
+            Vector2f(pTexCoord->x, pTexCoord->y),
+            Vector3f(pNormal->x, pNormal->y, pNormal->z));
+        Vertices.push_back(v); // добавляем в массив вершин
+    }
+    // заполняем индексный массив при помощи объекта aiFace, хранящего индексы вершин модели
+    for (unsigned int i = 0; i < paiMesh->mNumFaces; i++)
+    {
+        const aiFace& Face = paiMesh->mFaces[i];
+        assert(Face.mNumIndices == 3);
+        Indices.push_back(Face.mIndices[0]);
+        Indices.push_back(Face.mIndices[1]);
+        Indices.push_back(Face.mIndices[2]);
+    }
+    m_Entries[Index].Init(Vertices, Indices); // привязка (см Init)
+}
+// функция загружает все текстуры, которые используются в модели
+bool Mesh::InitMaterials(const aiScene* pScene, const std::string& Filename)
+{
+ //    Извлекаем директорию из полного имени файла
+    std::string::size_type SlashIndex = Filename.find_last_of("/");
+    std::string Dir;
+
+    if (SlashIndex == std::string::npos) 
+    {
+        Dir = ".";
+    }
+    else if (SlashIndex == 0) 
+    {
+        Dir = "/";
+    }
+    else
+    {
+        Dir = Filename.substr(0, SlashIndex);
+    }
+    bool Ret = true;
+
+    // Инициализируем материал
+    for (unsigned int i = 0; i < pScene->mNumMaterials; i++) 
+    {
+        const aiMaterial* pMaterial = pScene->mMaterials[i];
+
+        m_Textures[i] = NULL;
+        if (pMaterial->GetTextureCount(aiTextureType_DIFFUSE) > 0) 
+        {
+            aiString Path;
+            if (pMaterial->GetTexture(aiTextureType_DIFFUSE, 0, &Path,
+                NULL, NULL, NULL, NULL, NULL) == AI_SUCCESS) 
+            {
+                std::string FullPath = Dir + "/" + Path.data;
+               // std::cout << FullPath << " \n";
+               // FullPath = "cube.jpg";
+                m_Textures[i] = new Texture(GL_TEXTURE_2D, FullPath.c_str());
+
+                if (!m_Textures[i]->Load())
+                {
+                    printf("Error loading texture '%s'\n", FullPath.c_str());
+                    delete m_Textures[i];
+                    m_Textures[i] = NULL;
+                    Ret = false;
+                }
+                else
+                {
+                    printf("Loaded texture '%s'\n", FullPath.c_str());
+                }
+            }
+        }
+        // Загружаем белую текстуру если модель не имеет собственной
+       // if (!m_Textures[i])
+       // {
+        //    m_Textures[i] = new Texture(GL_TEXTURE_2D, "white.png");
+        //    Ret = m_Textures[i]->Load();
+       // }
+    }
+    return Ret;
+}
+// функция отрисовки
+void Mesh::Render()
+{
+    // включаем атрибуты массива вершин
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+    for (unsigned int i = 0; i < m_Entries.size(); i++) 
+    {
+        // указываем параметры каждого атрибута
+        glBindBuffer(GL_ARRAY_BUFFER, m_Entries[i].VB);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)12);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)20);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_Entries[i].IB);
+
+        const unsigned int MaterialIndex = m_Entries[i].MaterialIndex;
+        // привязываем // привязываем текстуры к конкретному модулю GL_TEXTURE
+        if (MaterialIndex < m_Textures.size() && m_Textures[MaterialIndex])
+        {
+            m_Textures[MaterialIndex]->Bind(GL_TEXTURE0);
+        }
+
+        glDrawElements(GL_TRIANGLES, m_Entries[i].NumIndices, GL_UNSIGNED_INT, 0); // рисуем треугольники
+    }
+    // выключаем атрибуты массива вершин
+    glDisableVertexAttribArray(2);
+    glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(0);
+}
