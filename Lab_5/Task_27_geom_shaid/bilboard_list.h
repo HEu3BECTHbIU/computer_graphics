a6e56117c87c0568ad69ebadee9a08569cc5d410
Demid <demidberdin1234@gmail.com>
2023-05-06 10:25:09 +0500

    Task 27 added

---
+++ Lab_5/Task_27_geom_shaid/bilboard_list.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <string>
+
+#include "texture.h"
+#include "bilboard_technique.h"
+// инкапсулирует все, что вам потребуется для генерации billboards
+class BillboardList 
+{
+public:
+    BillboardList();
+    ~BillboardList();
+
+    bool Init(const std::string& TexFilename);
+
+    void Render(const Matrix4f& VP, const Vector3f& CameraPos);
+
+private:
+    void CreatePositionBuffer();
+
+    GLuint m_VB; // вершинный буфер
+    Texture* m_pTexture; // текстура для билборда
+    BillboardTechnique m_technique;
+};
---
+++ Lab_5/Task_27_geom_shaid/technique.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <GL/glew.h>
+#include <list>
+
+#define INVALID_UNIFORM_LOCATION 0xFFFFFFFF
+class Technique // инкапсуляция добавления и компиляции шейдеров
+{
+public:
+    Technique();
+    ~Technique();
+    virtual bool Init();
+    void Enable();
+
+protected:
+    bool AddShader(GLenum ShaderType, const char* pShaderText);
+    bool Finalize();
+    GLint GetUniformLocation(const char* pUniformName);
+
+private:
+    GLuint m_shaderProg; // шейдерная программа
+    typedef std::list<GLuint> ShaderObjList; // лист шейдерных объектов
+    ShaderObjList m_shaderObjList;
+};
