3572207b37585cbeb2d28c8c15a2f3a03709d109
Demid <demidberdin1234@gmail.com>
2023-05-06 10:31:05 +0500

    Task 33 added

---
+++ Lab_5/Task_33_instance_render/lightening_technique.h
@@ -0,0 +1,144 @@
+﻿#ifndef LIGHTING_TECHNIQUE_H
+#define	LIGHTING_TECHNIQUE_H
+#include "technique.h"
+#include "math_3d.h"
+
+// #define MAX_POINT_LIGHTS 3
+
+struct BaseLight
+{
+    Vector3f Color; // цвет света
+    float AmbientIntensity; // интенсивность фонового
+    float DiffuseIntensity; // инетнсивность рассеянного
+
+    BaseLight()
+    {
+        Color = Vector3f(0.0f, 0.0f, 0.0f);
+        AmbientIntensity = 0.0f;
+        DiffuseIntensity = 0.0f;
+    }
+};
+
+struct DirectionalLight : public BaseLight // направленный свет
+{
+    Vector3f Direction; // направление
+
+    DirectionalLight()
+    {
+        Direction = Vector3f(0.0f, 0.0f, 0.0f);
+    }
+};
+
+struct PointLight : public BaseLight // точечный свет
+{
+    Vector3f Position;
+
+    struct
+    {
+        float Constant; // коэффициенты затухания
+        float Linear;
+        float Exp;
+    } Attenuation;
+
+    PointLight()
+    {
+        Position = Vector3f(0.0f, 0.0f, 0.0f);
+        Attenuation.Constant = 1.0f;
+        Attenuation.Linear = 0.0f;
+        Attenuation.Exp = 0.0f;
+    }
+};
+
+struct SpotLight : public PointLight // свет типа прожектор
+{
+    Vector3f Direction; // направление
+    float Cutoff; // радиус, после которого свет затухает
+
+    SpotLight()
+    {
+        Direction = Vector3f(0.0f, 0.0f, 0.0f);
+        Cutoff = 0.0f;
+    }
+};
+
+class LightingTechnique : public Technique // класс техники света
+{
+public:
+
+    static const unsigned int MAX_POINT_LIGHTS = 2; // максимальное количество точечных источников
+    static const unsigned int MAX_SPOT_LIGHTS = 2; // макс. количество прожекторов
+
+    LightingTechnique();
+
+    virtual bool Init();
+
+
+    void SetColorTextureUnit(unsigned int TextureUnit);
+
+    void SetColor(unsigned int Index, const Vector4f& Color);
+
+    void SetDirectionalLight(const DirectionalLight& Light);
+    void SetPointLights(unsigned int NumLights, const PointLight* pLights);
+    void SetSpotLights(unsigned int NumLights, const SpotLight* pLights);
+    void SetEyeWorldPos(const Vector3f& EyeWorldPos);
+    void SetMatSpecularIntensity(float Intensity);
+    void SetMatSpecularPower(float Power);
+
+private:
+    // переменные, в которые будет помещено местоположение юниформ переменных в шейдере
+
+    GLuint  m_colorTextureLocation;
+
+    GLuint m_eyeWorldPosLocation;
+    GLuint m_matSpecularIntensityLocation;
+    GLuint m_matSpecularPowerLocation;
+    GLuint m_numPointLightsLocation;
+    GLuint m_numSpotLightsLocation;
+
+    GLuint m_colorLocation[4];
+
+    // структура, в поля которой будут помещены местонахождения юниформ переменных
+    // соответствующей структуры в шейдере (направленный свет)
+    struct
+    {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint DiffuseIntensity;
+        GLuint Direction;
+    } m_dirLightLocation;
+
+    // аналогичный массив структур для точечных источников света 
+    struct
+    {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint DiffuseIntensity;
+        GLuint Position;
+        struct
+        {
+            GLuint Constant;
+            GLuint Linear;
+            GLuint Exp;
+        } Atten;
+    } m_pointLightsLocation[MAX_POINT_LIGHTS];
+
+    // аналогичный массив структур для прожекторов
+    struct
+    {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint DiffuseIntensity;
+        GLuint Position;
+        GLuint Direction;
+        GLuint Cutoff;
+        struct 
+        {
+            GLuint Constant;
+            GLuint Linear;
+            GLuint Exp;
+        } Atten;
+    } m_spotLightsLocation[MAX_SPOT_LIGHTS];
+};
+
+
+#endif	/* LIGHTING_TECHNIQUE_H */
